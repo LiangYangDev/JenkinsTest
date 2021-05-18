3e569ce25002750e0db090fa914b0bb3d111c8e5
Liangxuyang <liangxuyang@lyzdfintech.com>
2021-05-18 17:11:46 +0800

    提交修改

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Android jenkins build script starts"
+androidModule=$1
+echo "$androidModule module build starts"
+echo "$androidModule module build ends"
+echo "Android jenkins build script ends"
